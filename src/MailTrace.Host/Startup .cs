ea22b2330a493667329a6987dd0c446485460037
Mark Lopez <m@silvenga.com>
2016-07-06 22:45:55 -0500

    Added host project.

---
+++ src/MailTrace.Host/Startup .cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(MailTrace.Host.Startup))]
+
+namespace MailTrace.Host
+{
+    using System.Web.Http;
+
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureWebApi(app);
+        }
+
+        private static void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+        }
+    }
+}

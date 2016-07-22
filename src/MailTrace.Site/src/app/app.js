981004454b88a4fbd6c21f98ee7b4bbed91a77da
Mark Lopez <m@silvenga.com>
2016-07-22 00:10:11 -0500

    Configured VSCode.

--- src/MailTrace.Site/src/app/app.js
+++ src/MailTrace.Site/src/app/app.js
@@ -1,3 +1,13 @@
-export class App {
+import {
+    HttpClient
+} from 'aurelia-fetch-client';
 
+@inject(HttpClient)
+export class App {
+    constructor(http) {
+        http.configure(config => {
+            config.withBaseUrl('api/');
+        });
+        this.http = http;
+    }
 }
